394b322df36563dd9865c7ab682834f8bad70e73
Juan D. Acosta <juandacorias@gmail.com>
2017-07-19 16:28:09 -0500

    Added Redux and ESLint setup.

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initState = {
+  showPanel: false,
+};
+
+function siteApp(state = initState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default siteApp;
