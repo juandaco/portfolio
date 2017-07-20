acc987cd4d6598352262577d611158b2b1749fd2
Juan D. Acosta <juandacorias@gmail.com>
2017-07-20 05:57:12 -0500

    Temporarily finished Logo

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Title = () => (
+  <div className="title-container">
+    <h2 className="site-title logo">
+      <span id="logo-s">S</span>
+      ynt
+      <span id="logo-4">4</span>
+      RT
+    </h2>
+    <hr />
+  </div>
+);
+
+export default Title;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import Title from '../components/Title';
 
 class App extends Component {
   render() {
     return (
       <div className="site-main">
-        <div className="title-container">
-          <h2 className="site-title logo">
-            <span id="logo-s">S</span>
-            ynt
-            <span id="logo-4">4</span>
-            RT
-          </h2>
+        <div className="site-header">
+          <Title />
         </div>
         <p className="App-intro">Content to be defined.</p>
       </div>
