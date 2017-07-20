7cc8e1bc01753f3012c21f4f3ac0d8d6f8fdca91
Juan D. Acosta <juandacorias@gmail.com>
2017-07-19 19:41:04 -0500

    First Logo Sketch

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>My Portfolio</h2>
+      <div className="site-main">
+        <div className="title-container">
+          <h2 className="site-title logo">
+            <span id="logo-s">S</span>
+            ynt
+            <span id="logo-4">4</span>
+            RT
+          </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Content to be defined.</p>
       </div>
     );
   }
