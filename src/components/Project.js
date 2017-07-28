2c4c7db82611c2638028a7d0c002048d6116043f
Juan D. Acosta <juandacorias@gmail.com>
2017-07-28 06:56:10 -0500

    Responsive Title

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Project = ({ title, description, icon, link, repo, stack }) => {
   return (
-    <div className="project">
+    <div className="project" >
       <i className={`fa ${icon} fa-3x project-icon`} />
       <div className="project-content">
         <h3>
@@ -10,18 +10,18 @@ const Project = ({ title, description, icon, link, repo, stack }) => {
         </h3>
         <p dangerouslySetInnerHTML={{ __html: description }} />
         <div className="project-btn-container">
-        <a className="project-btn" href={repo} target="_blank">
-          <button>
-            Code
-            <i className="fa fa-code project-btn-icon" />
-          </button>
-        </a>
-        <a className="project-btn" href={link} target="_blank">
-          <button>
-            Project
-            <i className="fa fa-desktop project-btn-icon" />
-          </button>
-        </a>
+          <a className="project-btn" href={repo} target="_blank">
+            <button>
+              Code
+              <i className="fa fa-code project-btn-icon" />
+            </button>
+          </a>
+          <a className="project-btn" href={link} target="_blank">
+            <button>
+              Project
+              <i className="fa fa-desktop project-btn-icon" />
+            </button>
+          </a>
         </div>
       </div>
     </div>
